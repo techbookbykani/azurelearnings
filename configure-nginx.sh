ecd7940fdedca2b286b7e5a16ea4101be7d7f7fe
techbookbykani <techbookbykani@gmail.com>
2026-03-18 00:13:23 +0530

    Update configure-nginx.sh

--- configure-nginx.sh
+++ configure-nginx.sh
@@ -5,9 +5,13 @@ sudo apt-get update
 
 #We need to add repo before installing nginx-core. Otherwise we get an error
 sudo add-apt-repository main
+
 sudo add-apt-repository universe
+
 sudo add-apt-repository restricted
-sudo add-apt-repository multiverse  
+
+sudo add-apt-repository multiverse
+
 
 # Install Nginx.
 sudo apt-get install -y nginx
