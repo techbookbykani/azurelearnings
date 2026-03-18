a9100c4fd6fec2561752f29531b43aeb110bdf27
Kanimozhi M <Kanimozhi9052@gmail.com>
2026-03-18 19:31:37 +0530

    configuration for nginx

--- configure-nginx.sh
+++ configure-nginx.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
 # Update apt cache.
 sudo apt-get update
-
 #We need to add repo before installing nginx-core. Otherwise we get an error
 sudo add-apt-repository main
 sudo add-apt-repository universe
