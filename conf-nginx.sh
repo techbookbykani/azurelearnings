a1e850cecdbf1084129061ff67e61d68f4709b5a
Kanimozhi M <Kanimozhi9052@gmail.com>
2026-03-18 19:52:04 +0530

    configuration for nginx

--- conf-nginx.sh
+++ conf-nginx.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
 sudo apt-get update
-sudo add-apt-repository main
-sudo add-apt-repository universe
-sudo add-apt-repository restricted
-sudo add-apt-repository multiverse
-sudo apt-get install -y nginx
-echo "<html><body><h2>Welcome To TechbookbyKani! My hostname is $(hostname).</h2></body></html>" | sudo tee -a /var/www/html/index.html
